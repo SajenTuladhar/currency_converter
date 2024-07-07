78f95702fb52d64a76483c8c3653a6c4b0ec2def
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-07-07 15:46:04 +0545

    converter app complete

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,21 +1,35 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 
-class Home extends StatelessWidget {
+import 'package:flutter/material.dart';
+
+
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final TextEditingController _controller=  TextEditingController();
+
+  double result = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 43, 41, 41),
+        title: const Text('Currency Converter',style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+      ),
+      backgroundColor: const Color.fromARGB(255, 23, 22, 22),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              '0',
-              style: TextStyle(
+             Text(
+              result.toStringAsFixed(4),
+              style: const TextStyle(
                 color: Colors.white,
                 fontSize: 60,
               ),
@@ -23,11 +37,12 @@ class Home extends StatelessWidget {
             const SizedBox(
               height: 8,
             ),
-            const Padding(
-              padding: EdgeInsets.all(25),
+             Padding(
+              padding: const EdgeInsets.all(25),
               child: TextField(
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
+                controller: _controller,
+                style: const TextStyle(color: Colors.white),
+                decoration: const InputDecoration(
                   hintText: 'Input amount in USD',
                   hintStyle: TextStyle(color: Color.fromARGB(255, 100, 99, 99)),
                   border: OutlineInputBorder(
@@ -35,7 +50,7 @@ class Home extends StatelessWidget {
                       ),
                   prefixIcon: Icon(Icons.monetization_on_outlined),
                 ),
-                keyboardType: TextInputType.numberWithOptions(),
+                keyboardType: const TextInputType.numberWithOptions(),
               ),
             ),
             const SizedBox(
@@ -43,9 +58,14 @@ class Home extends StatelessWidget {
             ),
             ElevatedButton(
                 style: const ButtonStyle(
+                  shape: MaterialStatePropertyAll(RoundedRectangleBorder()),
                   fixedSize: MaterialStatePropertyAll(Size(200, 30))
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    result = double.parse (_controller.text) * 133;
+                  });
+                },
                 child: const Text(
                   'Convert',
                   style: TextStyle(fontSize: 16, color: Colors.black),
