680b16093d7401921b5caf7cfbce1532643c05cf
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-07-07 14:29:13 +0545

    button properties

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,45 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
-
 class Home extends StatelessWidget {
-  const Home ({super.key});
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black,
-      body:  Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('0', 
-            style: TextStyle(
-               color: Colors.white,
-               fontSize: 60,
-            ),),
-            const SizedBox(height: 20,),
+            const Text(
+              '0',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 60,
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
             const Padding(
               padding: EdgeInsets.all(25),
               child: TextField(
                 style: TextStyle(color: Colors.white),
                 decoration: InputDecoration(
-                   hintText: 'Input amount in USD',
-                   hintStyle: TextStyle(color: Color.fromARGB(255, 100, 99, 99)),
+                  hintText: 'Input amount in USD',
+                  hintStyle: TextStyle(color: Color.fromARGB(255, 100, 99, 99)),
                   border: OutlineInputBorder(
-                    //gapPadding: 100,
-                  ),
+                      //gapPadding: 100,
+                      ),
                   prefixIcon: Icon(Icons.monetization_on_outlined),
                 ),
                 keyboardType: TextInputType.numberWithOptions(),
               ),
-              
             ),
-            ElevatedButton(onPressed: () {}, child: const Text('Enter'))
+            const SizedBox(
+              height: 8,
+            ),
+            ElevatedButton(
+                style: const ButtonStyle(
+                  fixedSize: MaterialStatePropertyAll(Size(200, 30))
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'Convert',
+                  style: TextStyle(fontSize: 16, color: Colors.black),
+                ))
           ],
         ),
-        
-
       ),
     );
   }
